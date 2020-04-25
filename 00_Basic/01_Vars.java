148b112a8efa94393cb48926f694ac69443c80bb
dargyris <diamantisargiris@gmail.com>
2020-04-25 20:09:48 +0300

    Changing class names.

---
+++ 00_Basic/01_Vars.java
@@ -0,0 +1,25 @@
+class Vars {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, everyone!");
+
+        int myFirstVariable = 0;
+        System.out.println(myFirstVariable);
+        int mySecondVariable = 1;
+        System.out.println(mySecondVariable);
+        int myThirdVariable = 2;
+        System.out.println(myThirdVariable);
+        int myFourthVariable = 3;
+        System.out.println(myFourthVariable);
+        int myFifthVariable = 4;
+        System.out.println(myFifthVariable);
+        int mySixthVariable = 5;
+        System.out.println(mySixthVariable);
+        int mySeventhVariable = 6;
+        System.out.println(mySeventhVariable);
+        int myEighthVariable = 7;
+        System.out.println(myEighthVariable);
+        int myNinthVariable = 8;
+        System.out.println(myNinthVariable);
+    }
+}
