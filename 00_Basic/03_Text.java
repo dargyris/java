d2d42564b8e17b4b9113e7579a2e1e0936b69dfc
dargyris <diamantisargiris@gmail.com>
2020-04-26 13:28:05 +0300

    Adding ASCII Art to 03_Text.java.

--- 00_Basic/03_Text.java
+++ 00_Basic/03_Text.java
@@ -8,5 +8,15 @@ class Text {
         System.out.println("but change the line __in the code__,");
         System.out.println("the line changes in the output as well.\n");
         System.out.println("Do I like this?...\n");
+        System.out.println(" _                           _                    _                    _ "); 
+        System.out.println("| |                         (_)                  | |                  | |");
+        System.out.println("| |     ___  __ _ _ __ _ __  _ _ __   __ _       | | __ ___   ____ _  | |");
+        System.out.println("| |    / _ \/ _` | '__| '_ \| | '_ \ / _` |  _   | |/ _` \ \ / / _` | | |");
+        System.out.println("| |___|  __/ (_| | |  | | | | | | | | (_| | | |__| | (_| |\ V / (_| | |_|");
+        System.out.println("|______\___|\__,_|_|  |_| |_|_|_| |_|\__, |  \____/ \__,_| \_/ \__,_| (_)");
+        System.out.println("           __/ |                                                         ");
+        System.out.println("          |___/                                                          ");
+        System.out.println("=========================================================================");
+
     }
 }
