a12cb3a166e45d0e2fb75451b0c2a52d23cce385
dargyris <diamantisargiris@gmail.com>
2020-04-26 13:46:08 +0300

    Adding chickens to the repo.

---
+++ 00_Basic/04_Chicken.java
@@ -0,0 +1,17 @@
+class Chicken {
+    
+    public static void main(String[] args) {
+        System.out.println("\033[H\033[2J");
+        System.out.println("Cluck!\n");
+        System.out.println("      /'._     _,     ");  
+        System.out.println("      \\   ;__.'  }   ");
+        System.out.println("   (`-._;-\" _.--.}'   ");
+        System.out.println("   /_'    /`    _}    ");
+        System.out.println("     `.  \\_._.;      ");
+        System.out.println("       '-.__ /        ");
+        System.out.println("        _/  `\\       ");
+        System.out.println("        ^`   ^`       ");
+        System.out.println("\n");
+    }
+
+}
