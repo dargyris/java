35502f4a51d0ca6422a160811422a88161a9515b
dargyris <diamantisargiris@gmail.com>
2020-04-27 12:24:01 +0300

    Pounds to Kg with java.

---
+++ 00_Basic/15_PoundsKg.java
@@ -0,0 +1,12 @@
+class PoundsKg{
+    public static void main(String[] args){
+    System.out.print("\033[H\033[2J");
+
+    double Pounds = 10D;
+    double Kilograms = Pounds * 0.45359237;
+    
+    System.out.println("Pounds\t: " + Pounds);
+    System.out.println("Kilogr.\t: " + Kilograms + "\n");
+
+    }
+} 
