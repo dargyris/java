1d749ba43c0f007acd62a50dd0f884e1cf043c8d
dargyris <diamantisargiris@gmail.com>
2020-04-27 17:27:33 +0300

    = vs ==.

---
+++ 00_Basic/23_EqualAssign.java
@@ -0,0 +1,17 @@
+class EqualAssign{
+    public static void main(String[] args){
+    System.out.print("\033[H\033[2J");
+
+    int newValue = 50;
+    if(newValue == 50){
+        System.out.println("This was an error.\n");
+        }
+
+    boolean isCar = false;
+    if (isCar == true)
+        System.out.println("This is a nice car!\n");
+    else 
+        System.out.println("This is not a car.\n");
+
+    }
+}
