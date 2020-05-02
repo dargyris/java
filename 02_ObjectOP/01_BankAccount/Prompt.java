215520181566ff628fe6bee9215dedc872b20a70
dargyris <diamantisargiris@gmail.com>
2020-05-02 23:25:30 +0300

    Handling input here instead of Menu.

--- 02_ObjectOP/01_BankAccount/Prompt.java
+++ 02_ObjectOP/01_BankAccount/Prompt.java
@@ -1,28 +1,26 @@
 class Prompt{
 
-    public static void prompt( int threeTries ){
+    public static int prompt( int threeTries ){
 
+        int intChoice = -1;
         switch ( threeTries ) {
-            case 0:
-                System.out.print( "> " );
-                break;
-            case 1:
-                System.out.println( "Unknown choice." );
-                System.out.println( "Please try again..." );
-                System.out.println( "( Rem. tries: 2 )" );
-                System.out.print( "> " );
-                break;
-            case 2:
-                System.out.println( "Unknown choice." );
-                System.out.println( "Please try again..." );
-                System.out.println( "( Rem. tries: 1 )" );
-                System.out.print( "> " );
-                break;
             case 3:
-                System.out.println( "Reached max number of tries." );
-                System.out.println( "Quitting...\n" );
+                System.out.print( "\t> " );
+                intChoice = GetInput.getInput();
+                break;
+            case 2: case 1:
+                System.out.println( "\tUnknown choice. Please try again." );
+                System.out.println( "\t( Rem. tries: " + threeTries + "/3 )\t" );
+                System.out.print( "\t> " );
+                intChoice = GetInput.getInput();
+                break;
+            case 0:
+                System.out.println( "\tReached max number of tries." );
+                System.out.println( "\tQuitting...\n" );
                 break;
         }
+
+        return intChoice;
 
     }
 
