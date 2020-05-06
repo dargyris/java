f123c911972c477b3660bf4084f3cb317077ba53
dargyris <diamantisargiris@gmail.com>
2020-05-06 17:57:10 +0300

    Returns an int. 0 if successful. -1 if failed.

--- 02_ObjectOP/01_BankAccount/Deposit.java
+++ 02_ObjectOP/01_BankAccount/Deposit.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 
 class Deposit{
 
-    public static Account deposit( Account theAccount ){
+    public static int deposit( Account theAccount ){
+        // int: If it succeeds returns 0. Otherwise -1.
+        
+        int status = -1;
+        // success variable. 0 if successful.
 
         Scanner scanner = new Scanner ( System.in );
         int threeTries = 3;
@@ -13,12 +17,14 @@ class Deposit{
             System.out.println( "\t\t~ Deposit Menu ~" );
             System.out.println( "\t------------------------------------" );
             if ( theAccount.getCustomerName() == null ){
+                // If: There is no account
                 System.out.println( "\n\t>> No account present." );
                 System.out.println( "\t====================================" );
                 break;
             }
             System.out.println( "\tCurrent balance: " + theAccount.getBalance() );
             if ( threeTries < 3 ){
+                // If: Prompt with error message after 1st failed attempt.
                 System.out.println( "\n\tWrong input. Please try again." );
                 System.out.println( "\t( Rem. tries: " + threeTries + "/3 )" );
             }
@@ -32,12 +38,13 @@ class Deposit{
                 if ( myDouble >= 0 ){
                     theAccount.deposit( myDouble );
                     System.out.println( "\t------------------------------------" );
-                    System.out.println( "\n\n\tAmount: " + myDouble ); 
+                    System.out.println( "\tAmount: " + myDouble ); 
                     System.out.println( "\thas been successfully deposited." );
                     System.out.println( "\tYour new balance is: " + 
                             theAccount.getBalance() );
                     System.out.println( "\t====================================" );
-                    return theAccount;
+                    status = 0;
+                    break;
                 } else  {
                     scanner.nextLine();
                 }
@@ -46,7 +53,16 @@ class Deposit{
             }
             threeTries--;
         }
-        return theAccount;
+        if ( threeTries == 0 ){
+            // Deposit unsuccessful. Print fail message.
+            System.out.println( "\t------------------------------------" );
+            System.out.println( "\tMax number of tries reached." );
+            System.out.println( "\t====================================" );
+            System.out.print( "\n\tReturning to main menu" );
+        }
+        // Wait 4 seconds and printing wait characters.
+        Relax.relax();
+        return status;      // int: 0 if successful. -1 if not.
     }
 
 }
