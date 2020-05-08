019f72bb648b2e6ad79b9c955a032778c9a2068b
dargyris <diamantisargiris@gmail.com>
2020-05-08 19:18:41 +0300

    Testing constructor overloading.

--- 02_ObjectOP/02_BankAccountSimple/Main.java
+++ 02_ObjectOP/02_BankAccountSimple/Main.java
@@ -2,20 +2,21 @@ public class Main {
 
     public static void main ( String[] args ){
         System.out.print("\033[H\033[2J");
+        
+        Account bobsAccount = new Account(); //"1234", 0., "Bob Brown", 
+                // "email@email.com", "(090) 242-4009");
 
-        Account bobsAccount = new Account("1234", 0., "Bob Brown", 
-                "email@email.com", "(090) 242-4009");
-
+        System.out.println( "Number\t:" + bobsAccount.getNumber() );
+        System.out.println( "Balance\t: " + bobsAccount.getBalance() );
         bobsAccount.withdrawal(100.0);
         bobsAccount.deposit(50.);
         bobsAccount.withdrawal(100.);
         bobsAccount.deposit(51.);
         bobsAccount.withdrawal(100.);
         
-        System.out.println( "Name\t:" + bobsAccount.getCustomerName());
-        System.out.println( "Number\t:" + bobsAccount.getNumber() );
-        System.out.println( "Balance\t:" + bobsAccount.getCustomerEmail());
-        System.out.println( "Phone\t:" + bobsAccount.getCustomerPhone );
+//        System.out.println( "Name\t:" + bobsAccount.getCustomerName());
+//        System.out.println( "Balance\t:" + bobsAccount.getCustomerEmail() );
+//        System.out.println( "Phone\t:" + bobsAccount.getCustomerPhone() );
     }
 
 }
