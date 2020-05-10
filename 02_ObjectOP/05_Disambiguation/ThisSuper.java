e0cd0549e21b0a09ed80cd824b4b58e8f08b07be
dargyris <diamantisargiris@gmail.com>
2020-05-10 20:34:44 +0300

    An enlightening demonstration of this vs super.

--- 02_ObjectOP/05_Disambiguation/ThisSuper.java
+++ 02_ObjectOP/05_Disambiguation/ThisSuper.java
@@ -1,24 +1,134 @@
-public class ThisSuper {
+class ThisVsSuperParent {
 
-    public void whatSuperDoes(){
-        System.out.println( "To access/call the parent class members, variables and methods." );
-        System.out.println( "When I am defining a function that has the same name" );
-        System.out.println( "as another function belonging to the parent class." );
+    private int itsParentFirstVar;
+    private int itsParentSecondVar;
+    private int itsParentThirdVar;
 
-        System.out.println( "\nThe only way to call a parent constructor is with super()." );
+    public ThisVsSuperParent(){
+        this(40, 40, 40);
+        System.out.println( "~ Empty (40 40 40) Parent Constructor ~" );
     }
 
-    public void whatThisDoes(){
-        System.out.println( "Call current class members, variables and methods." );
-        System.out.println( "Required when I am using parameters with the same name" ); 
-        System.out.println( "as instance variables." );
-        System.out.println( "For demonstration purposes for instances I am using the" );
-        System.out.println( "prefix \"its\" for its Vars." );
-        System.out.println( "I can use both in a class except in static areas." );
-        System.out.println( "This is used with setters and optionally with getters." );
-        
-        System.out.println( "\nWe use this() to call a constructor from another" );
-        System.out.println( "overloaded constructor in the same class." );
+    public ThisVsSuperParent(int parentFirstVar){
+        this(parentFirstVar, 40, 40);
+        System.out.println( "~ 40, 40 Parent Constructor ~" );
+    }
+
+    public ThisVsSuperParent(int parentFirstVar,
+                             int parentSecondVar){
+        this(parentFirstVar, parentSecondVar, 40);
+        System.out.println( "~ 40 Parent Constructor ~" );
+    }
+
+    public ThisVsSuperParent( int parentFirstVar,
+                            int parentSecondVar,
+                            int parentThirdVar){
+        this.itsParentFirstVar = parentFirstVar;
+        this.itsParentSecondVar = parentSecondVar;
+        this.itsParentThirdVar = parentThirdVar;
+        System.out.println( "~ Parent Setter Constructor ~" );
+    }
+
+    public void printMethod(){
+        System.out.println( "> Parent First Var\t: " + this.itsParentFirstVar );
+        System.out.println( "> Parent Second Var\t: " + this.itsParentSecondVar );
+        System.out.println( "> Parent Third Var\t: " + this.itsParentThirdVar );
+    }
+
+    public void setItsParentFirstVar(int parentFirstVar){
+        this.itsParentFirstVar = parentFirstVar;
+    }
+
+    public void setItsParentSecondVar(int parentSecondVar){
+        this.itsParentSecondVar = parentSecondVar;
+    }
+
+    public void setItsParentThirdVar(int parentThirdVar){
+        this.itsParentThirdVar = parentThirdVar;
+    }
+
+    public int getItsParentFirstVar(){
+        return this.itsParentFirstVar;
+    }
+
+    public int getItsParentSecondVar(){
+        return this.itsParentSecondVar;
+    }
+
+    public int getItsParentThirdVar(){
+        return this.itsParentThirdVar;
+    }
+}
+
+class ThisVsSuperChild extends ThisVsSuperParent {
+
+    private short itsChildFirstVar;
+    private short itsChildSecondVar;
+    private short itsChildThirdVar;
+
+    public ThisVsSuperChild() {
+ //       this((short)5, (short)5, (short)5);
+ //       super.setItsParentFirstVar(5);
+ //       super.setItsParentSecondVar(5);
+ //       super.setItsParentThirdVar(5);
+        super(5, 5, 5);
+        this.setItsChildFirstVar((short)5);
+        this.setItsChildSecondVar((short)5);
+        this.setItsChildThirdVar((short)5);
+        System.out.println( "\t~ Empty (5 5 5) Child Constructor ~" );
+    }
+
+    public ThisVsSuperChild( short childFirstVar){
+        this(childFirstVar, (short)5, (short)5);
+        System.out.println( "\t~ 5 5 Child Constructor ~" );
+    }
+
+    public ThisVsSuperChild( short childFirstVar,
+                             short childSecondVar){
+        this(childFirstVar, childSecondVar, (short)5);
+        System.out.println( "\t~ 5 Child Constructor ~" );
+    }
+
+    public ThisVsSuperChild( short childFirstVar,
+                             short childSecondVar,
+                             short childThirdVar){
+
+        this.itsChildFirstVar = childFirstVar;
+        this.itsChildSecondVar = childSecondVar;
+        this.itsChildThirdVar = childThirdVar;
+        System.out.println( "\t~ Setter Child Constructor ~" );
+    }
+    
+    @Override
+    public void printMethod(){
+        super.printMethod();
+        System.out.println( "\t> Child First Var\t: " + this.itsChildFirstVar );
+        System.out.println( "\t> Child Second Var\t: " + this.itsChildSecondVar );
+        System.out.println( "\t> Child Third Var\t: " + this.itsChildThirdVar );
+    }
+
+    public void setItsChildFirstVar(short childFirstVar){
+        this.itsChildFirstVar = childFirstVar;
+    }
+
+    public void setItsChildSecondVar(short childSecondVar){
+        this.itsChildSecondVar = childSecondVar;
+    }
+
+    public void setItsChildThirdVar(short childThirdVar){
+        this.itsChildThirdVar = childThirdVar;
+    }
+
+    public short getItsChildFirstVar(){
+        return (short)this.itsChildFirstVar;
+    }
+
+    public short getItsChildSecondVar(){
+        return (short)this.itsChildSecondVar;
+    }
+
+    public short getItsChildThirdVar(){
+        return (short)this.itsChildThirdVar;
     }
 
 }
