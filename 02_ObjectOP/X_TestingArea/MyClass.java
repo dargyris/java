e17fc9d2e33b99b2eb9ac6b6aa4d342b0d4f3e16
dargyris <diamantisargiris@gmail.com>
2020-05-11 23:17:14 +0300

    Testing my automatic getter/setter generator!

---
+++ 02_ObjectOP/X_TestingArea/MyClass.java
@@ -0,0 +1,89 @@
+//Testing Vim-script!
+public class MyClass {
+
+    private int itsFirstVar;
+    private int itsSecondVar;
+    private int itsThirdVar;
+    private short itsFourthVar;
+    private long itsFifthVar;
+    private String itsSixthVar;
+    protected bool itsSeventhVar;
+    protected String itsEighthVar;
+    public byte itsNinthVar;
+
+
+    public int getItsFirstVar(){
+		return this.itsFirstVar;
+	}
+
+    public int getItsSecondVar(){
+		return this.itsSecondVar;
+	}
+
+    public int getItsThirdVar(){
+		return this.itsThirdVar;
+	}
+
+    public short getItsFourthVar(){
+		return this.itsFourthVar;
+	}
+
+    public long getItsFifthVar(){
+		return this.itsFifthVar;
+	}
+
+    public String getItsSixthVar(){
+		return this.itsSixthVar;
+	}
+
+    public bool getItsSeventhVar(){
+		return this.itsSeventhVar;
+	}
+
+    public String getItsEighthVar(){
+		return this.itsEighthVar;
+	}
+
+    public byte getItsNinthVar(){
+		return this.itsNinthVar;
+	}
+
+
+    public void setItsFirstVar(int firstVar){
+		this.itsFirstVar = firstVar;
+	}
+
+    public void setItsSecondVar(int secondVar){
+		this.itsSecondVar = secondVar;
+	}
+
+    public void setItsThirdVar(int thirdVar){
+		this.itsThirdVar = thirdVar;
+	}
+
+    public void setItsFourthVar(short fourthVar){
+		this.itsFourthVar = fourthVar;
+	}
+
+    public void setItsFifthVar(long fifthVar){
+		this.itsFifthVar = fifthVar;
+	}
+
+    public void setItsSixthVar(String sixthVar){
+		this.itsSixthVar = sixthVar;
+	}
+
+    public void setItsSeventhVar(bool seventhVar){
+		this.itsSeventhVar = seventhVar;
+	}
+
+    public void setItsEighthVar(String eighthVar){
+		this.itsEighthVar = eighthVar;
+	}
+
+    public void setItsNinthVar(byte ninthVar){
+		this.itsNinthVar = ninthVar;
+	}
+
+
+}
