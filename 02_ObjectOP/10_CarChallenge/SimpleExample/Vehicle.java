1e438a52bebf384b345940ea93b5611a103fb524
dargyris <diamantisargiris@gmail.com>
2020-05-12 22:33:39 +0300

    Inheritance review.

--- 02_ObjectOP/10_CarChallenge/SimpleExample/Vehicle.java
+++ 02_ObjectOP/10_CarChallenge/SimpleExample/Vehicle.java
@@ -1,27 +1,12 @@
 public class Vehicle {
 
     private String itsName;
-    private int itsNumber;
-    private int itsOtherNumber;
-    private short itsDick;
-    private long itsPussy;
-    private boolean itsPoolean;
 
-    // =============== Getters ===============
+    // ========== Setter Contructor ==========
 
-    public Vehicle( String name,
-                    int number,
-                    int otherNumber,
-                    short dick,
-                    long pussy,
-                    boolean poolean){
+    public Vehicle( String name){
 
     this.itsName = name;
-    this.itsNumber = number;
-    this.itsOtherNumber = otherNumber;
-    this.itsDick = dick;
-    this.itsPussy = pussy;
-    this.itsPoolean = poolean;
 
     }
 
@@ -32,51 +17,11 @@ public class Vehicle {
 		this.itsName = name;
 	}
 
-    public void setItsNumber(int number){
-		this.itsNumber = number;
-	}
-
-    public void setItsOtherNumber(int otherNumber){
-		this.itsOtherNumber = otherNumber;
-	}
-
-    public void setItsDick(short dick){
-		this.itsDick = dick;
-	}
-
-    public void setItsPussy(long pussy){
-		this.itsPussy = pussy;
-	}
-
-    public void setItsPoolean(boolean poolean){
-		this.itsPoolean = poolean;
-	}
-
 
     // =============== Getters ===============
 
     public String getItsName(){
 		return this.itsName;
-	}
-
-    public int getItsNumber(){
-		return this.itsNumber;
-	}
-
-    public int getItsOtherNumber(){
-		return this.itsOtherNumber;
-	}
-
-    public short getItsDick(){
-		return this.itsDick;
-	}
-
-    public long getItsPussy(){
-		return this.itsPussy;
-	}
-
-    public boolean getItsPoolean(){
-		return this.itsPoolean;
 	}
 
 
