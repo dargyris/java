de8ba9cbb192ebc2addde614b42f51b99aaa01a1
dargyris <diamantisargiris@gmail.com>
2020-05-12 22:32:04 +0300

    Composition: putting it all together.

---
+++ 02_ObjectOP/11_Composition/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main ( String[] args ){
+
+        System.out.print("\033[H\033[2J");
+
+        Dimensions theDimensions = new Dimensions(20, 20, 5);
+        Case theCase = new Case("220B", "Dell", "240", theDimensions);
+
+        Monitor theMonitor = new Monitor("27inch Beash", "Acer", 27, new Resolution(2540, 1440));
+
+        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
+        thePC.getItsMonitor().drawPixelAt(1500, 1200, "red");
+        thePC.getItsMotherboard().loadProgram("Windows 1.0");
+        thePC.getItsEnclosure().pressPowerButton();
+
+    }
+
+}
