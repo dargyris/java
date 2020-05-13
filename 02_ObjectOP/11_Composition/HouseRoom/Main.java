5e5a0e9631f555dc3c84c5196ae28d42a373c716
dargyris <diamantisargiris@gmail.com>
2020-05-13 20:57:09 +0300

    Housekeeping.

--- 02_ObjectOP/11_Composition/HouseRoom/Main.java
+++ 02_ObjectOP/11_Composition/HouseRoom/Main.java
@@ -3,13 +3,43 @@ public class Main {
     public static void main ( String[] args ){
 
         System.out.print("\033[H\033[2J");
+        System.out.println( "\n" );
 
+        AbstractProperties theAbstractProperties = new AbstractProperties();
+        Dimensions theDimensions = new Dimensions();
+        PhysicalProperties thePhysicalProperties = new PhysicalProperties();
+        Chair theChair = new Chair();
+        Desk theDesk = new Desk();
+        Door theDoor = new Door();
+        Window theWindow = new Window();
+        Tiles theTiles = new Tiles();
+        Floor theFloor = new Floor();
         Room theRoom = new Room();
-        theRoom.printValues();
-        System.out.println();
-        HouseRoom theHouseRoom = new HouseRoom();
-        theHouseRoom.printValues();
+        PhysicalSpace thePhysicalSpace = new PhysicalSpace();
 
+        // PRINTING =========================================================
+        theDimensions.printContent();
+        System.out.println();
+        thePhysicalProperties.printContent();
+        System.out.println();
+        theAbstractProperties.printContent();
+        System.out.println();
+        theChair.printContent();
+        System.out.println();
+        theDesk.printContent();
+        System.out.println();
+        theDoor.printContent();
+        System.out.println();
+        theWindow.printContent();
+        System.out.println();
+        theTiles.printContent();
+        System.out.println();
+        theFloor.printContent();
+        System.out.println();
+        theRoom.printContent();
+        System.out.println();
+        thePhysicalSpace.printContent();
+        System.out.println();
 
     }
 
--- 02_ObjectOP/11_Composition/HouseRoom/Room.java
+++ 02_ObjectOP/11_Composition/HouseRoom/Room.java
@@ -1,61 +1,143 @@
-public class Room {
+public class Room extends PhysicalSpace{
+    
+    private Dimensions itsDimensions;
+    private Chair itsChair;
+    private Desk itsDesk;
+    private Door itsDoor;
+    private Window itsWindow;
+    private Floor itsFloor;
 
-    private String itsType;
-    private int itsArea;
-    private int itsHeight;
+    // =======================================
+
+    public Room(){
+        this((short)9, "Room",
+//             new AbstractProperties((short)9, "RoomA"), // !b AbstractProperties implementation
+
+             new Dimensions(2, 2, 2), 
+             new Chair( new Dimensions(2, 2, 2),
+                        new PhysicalProperties(2, 2, "Room", "Room"),
+                        new AbstractProperties((short)2, "Room") 
+                        ),
+             new Desk ( new Dimensions(2, 2, 2),
+                        new PhysicalProperties(2, 2, "Room", "Room"),
+                        new AbstractProperties((short)2, "Room") 
+                        ),
+             new Door ( new Dimensions(2, 2, 2),
+                        new PhysicalProperties(2, 2, "Room", "Room"),
+                        new AbstractProperties((short)2, "Room") 
+                        ),
+             new Window( new Dimensions(2, 2, 2),
+                         new PhysicalProperties(2, 2, "Room", "Room"),
+                         new AbstractProperties((short)2, "Room") 
+                         ),
+             new Floor( 
+                 new Tiles( new Dimensions(3, 3, 3),
+                            new PhysicalProperties(3, 3, "Room", "Room"), 
+                            new AbstractProperties((short)3, "Room") 
+                            )
+                      )
+                 );
+ 
+    }
 
     // ========== Setter Contructor ==========
 
-    public Room(){
-        this("Null", 0, 0);
-    }
+    public Room( short volume,
+                 String use,
+//                 AbstractProperties properties,  // !b AbstractProperties implementation
 
-    public Room( String type,
-                 int area,
-                 int height){
-
-    	this.itsType = type;
-    	this.itsArea = area;
-    	this.itsHeight = height;
+                 Dimensions dimensions,
+                 Chair chair,
+                 Desk desk,
+                 Door door,
+                 Window window,
+                 Floor floor){
+        
+        super(volume, use, new AbstractProperties((short)9, "RoomA"));
+//        super(volume, use, properties);  // !b AbstractProperties implementation
+    	this.itsDimensions = dimensions;
+    	this.itsChair = chair;
+    	this.itsDesk = desk;
+    	this.itsDoor = door;
+    	this.itsWindow = window;
+    	this.itsFloor = floor;
 
     }
 
     // =======================================
 
-    public void printValues(){
-        System.out.println( "Type\t: " + this.itsType );
-        System.out.println( "Area\t: " + this.itsArea );
-        System.out.println( "Height\t: " + this.itsHeight );
+    @Override
+    public void printContent(){
+        System.out.println( "Room --> " );
+        this.itsChair.printContent();
+        System.out.println();
+        System.out.println( "Room --> " );
+        this.itsDesk.printContent();
+        System.out.println();
+        System.out.println( "Room --> " );
+        this.itsDoor.printContent();
+        System.out.println();
+        System.out.println( "Room --> " );
+        this.itsWindow.printContent();
+        System.out.println();
+        System.out.println( "Room --> " );
+        this.itsFloor.printContent();
+        System.out.println();
     }
 
     // =============== Setters ===============
 
-    public void setItsType(String type){
-		this.itsType = type;
+    public void setItsDimensions(Dimensions dimensions){
+		this.itsDimensions = dimensions;
 	}
 
-    public void setItsArea(int area){
-		this.itsArea = area;
+    public void setItsChair(Chair chair){
+		this.itsChair = chair;
 	}
 
-    public void setItsHeight(int height){
-		this.itsHeight = height;
+    public void setItsDesk(Desk desk){
+		this.itsDesk = desk;
+	}
+
+    public void setItsDoor(Door door){
+		this.itsDoor = door;
+	}
+
+    public void setItsWindow(Window window){
+		this.itsWindow = window;
+	}
+
+    public void setItsFloor(Floor floor){
+		this.itsFloor = floor;
 	}
 
 
     // =============== Getters ===============
 
-    public String getItsType(){
-		return this.itsType;
+    public Dimensions getItsDimensions(){
+		return this.itsDimensions;
 	}
 
-    public int getItsArea(){
-		return this.itsArea;
+    public Chair getItsChair(){
+		return this.itsChair;
 	}
 
-    public int getItsHeight(){
-		return this.itsHeight;
+    public Desk getItsDesk(){
+		return this.itsDesk;
 	}
+
+    public Door getItsDoor(){
+		return this.itsDoor;
+	}
+
+    public Window getItsWindow(){
+		return this.itsWindow;
+	}
+
+    public Floor getItsFloor(){
+		return this.itsFloor;
+	}
+
 
 
 }
