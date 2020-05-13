70082095f758a9e82e57ad112f95d5d1cbc04ba7
dargyris <diamantisargiris@gmail.com>
2020-05-13 13:58:52 +0300

    Simple in-game ASCII art.

---
+++ 02_ObjectOP/10_CarChallenge/ProceduralApproach/GameArt.java
@@ -0,0 +1,21 @@
+public class GameArt{
+
+    public static void gameArt(){
+    System.out.print("\033[H\033[2J");    
+    System.out.println( "\t================================" );
+    System.out.println( "\t                                " );
+    System.out.println( "\t                                " );
+    System.out.println( "\t            _________           " ); 
+    System.out.println( "\t           |         |          " ); 
+    System.out.println( "\t     _o_  _| ___=___ |_  _o_    " ); 
+    System.out.println( "\t    /`-'ooo         ooo /`-'\\   " ); 
+    System.out.println( "\t    |   |-|  ___*___  |-|   |   " ); 
+    System.out.println( "\t    |   | | (-+-+-+-) | |   |   " ); 
+    System.out.println( "\t    |   |--+_DARG IV_+--|   |   " ); 
+    System.out.println( "\t     ---                 ---    " ); 
+    System.out.println( "\t                                " );
+    System.out.println( "\t================================" );
+
+    }
+
+}
