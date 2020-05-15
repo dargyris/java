0558319bbdf6483c8b40c2f509a1f4a3eb85849a
dargyris <diamantisargiris@gmail.com>
2020-05-15 20:07:03 +0300

    A class for various types of veggies.

---
+++ 02_ObjectOP/14_OOP/Master/Veggie.java
@@ -0,0 +1,94 @@
+class FishermanLB extends Entity{
+
+    public FishermanLB(){
+        this("Natura\u2122 Fisherman's Last Breath", "Vegetable", 10.d, (short)0);
+    }
+
+    public FishermanLB( String name,
+                        String type,
+                        double price,
+                        short qty){
+        
+        super(name, type, price, qty);
+    }
+
+//     Setters and Getters it inherits from super-class Entity
+//     Also print functionality with the inherited printProperties();
+
+}
+
+class RhinoBV extends Entity{
+
+    public RhinoBV(){
+        this("B.T.O.\u2122 Baby Rhino Rare Greens", "Vegetable", 2.5d, (short)0);
+    }
+
+    public RhinoBV( String name,
+                    String type,
+                    double price,
+                    short qty){
+        
+        super(name, type, price, qty);
+    }
+
+//     Setters and Getters it inherits from super-class Entity
+//     Also print functionality with the inherited printProperties();
+
+}
+
+class SpaceAlgae extends Entity{
+
+    public SpaceAlgae(){
+        this("Natura\u2122 Space Algae", "Vegetable", 4.d, (short)0);
+    }
+
+    public SpaceAlgae( String name,
+                       String type,
+                       double price,
+                       short qty){
+        
+        super(name, type, price, qty);
+    }
+
+//     Setters and Getters it inherits from super-class Entity
+//     Also print functionality with the inherited printProperties();
+
+}
+
+class SeahorseUF extends Entity{
+
+    public SeahorseUF(){
+        this("Natura\u2122 Seahorse Unique Food", "Vegetable", 5.5d, (short)0);
+    }
+
+    public SeahorseUF( String name,
+                      String type,
+                      double price,
+                      short qty){
+        
+        super(name, type, price, qty);
+    }
+
+//     Setters and Getters it inherits from super-class Entity
+//     Also print functionality with the inherited printProperties();
+
+}
+
+class BlinkingSC extends Entity{
+
+    public BlinkingSC(){
+        this("B.T.O.\u2122 Blinking Sea-carrot", "Vegetable", 9.d, (short)0);
+    }
+
+    public BlinkingSC( String name,
+                     String type,
+                     double price,
+                     short qty){
+        
+        super(name, type, price, qty);
+    }
+
+//     Setters and Getters it inherits from super-class Entity
+//     Also print functionality with the inherited printProperties();
+
+}
