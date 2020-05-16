a4cb7bf09833580b1ffae9eb2c09ba64cce46b50
dargyris <diamantisargiris@gmail.com>
2020-05-16 20:10:38 +0300

    Putting it all together.

--- 02_ObjectOP/14_OOP/Master/Main.java
+++ 02_ObjectOP/14_OOP/Master/Main.java
@@ -1,7 +1,5 @@
 public class Main {
 
-    public static short REORDER = (short)10;
-
     public static short LOGO = (short)0;
     public static short BURGER = (short)1;
     public static short DELUXE = (short)2;
@@ -14,41 +12,66 @@ public class Main {
         Art theArt = new Art();
 
         Menu theMenu = new Menu();
-        short menuChoice = REORDER;
         boolean mainFlag = true;
+        boolean orderAgain = true;
+        short menuChoice = -1;
+        boolean hasOrdered = false;
         while(mainFlag){
-
             theArt.printArt(LOGO);
-            if(menuChoice == REORDER){              // REORDER -> (short) 10
-                menuChoice = theMenu.printMenu();
+            if(orderAgain){
+                menuChoice = (short)theMenu.printMenu();
             }
             switch(menuChoice){
                 case 0:
                     theArt.printArt(LOGO);
-                    System.out.println( "\nSunrise Burger\u00A9 will stay here even if you go now." );
-                    theArt.interact();
+                    System.out.println( "\n\t\tSunrise Burger\u00A9 will stay here even if you go now." );
+                    System.out.println();
+                    theArt.wait((short)50);
                     mainFlag = false;
                     break;
                 case 1:
                     Hamburger theHamburger = new Hamburger();
-                    boolean hasOrdered = theHamburger.order();
+                    hasOrdered = theHamburger.order();
                     if(hasOrdered){
-                        menuChoice = (short)theHamburger.serve();
+                        theHamburger.printReceipt();
+                        orderAgain = theHamburger.serve();
+                        if(orderAgain){
+                            break;
+                        }
+                        menuChoice = 0;
                         break;
                     } 
+                    orderAgain = false;
+                    menuChoice = -1;
                     break;
                 case 2:
-//                    Natura theNatura = new Natura();
-//                    menuChoice = (short)theNatura.serve();
+                    Natura theNatura = new Natura();
+                    hasOrdered = theNatura.order();
+                    if(hasOrdered){
+                        theNatura.printReceipt();
+                        orderAgain = theNatura.serve();
+                        if(orderAgain){
+                            break;
+                        }
+                        menuChoice = 0;
+                        break;
+                    }
+                    orderAgain = false;
+                    menuChoice = -1;
                     break;
                 case 3:
-//                    Deluxe theDeluxe = new Deluxe();
-//                    menuChoice = (short)theDeluxe.serve();
+                    Deluxe theDeluxe = new Deluxe();
+                    theDeluxe.printReceipt();
+                    orderAgain = theDeluxe.serve();
+                    if(orderAgain){
+                        break;
+                    }
+                    menuChoice = 0;
                     break;
                 default:
                     theArt.printArt(LOGO);
-                    System.out.println( "At Sinrise Burger\u00A9 silliness will not be tolerated!" );
-                    System.out.println( "Come back when you're really hungry. (If you want.)" );
+                    System.out.println( "\t\tAt Sunrise Burger\u00A9 silliness will not be tolerated!" );
+                    System.out.println( "\t\tCome back when you're really hungry. (If you want.)" );
                     theArt.wait((short)100);
                     System.out.println();
                     mainFlag = false;
