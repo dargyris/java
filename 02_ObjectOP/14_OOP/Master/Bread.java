ed8ce240e3ed207e558ff4cf5ec5bf8d036066bd
dargyris <diamantisargiris@gmail.com>
2020-05-16 20:05:51 +0300

    Classes for different types of breads.

--- 02_ObjectOP/14_OOP/Master/Bread.java
+++ 02_ObjectOP/14_OOP/Master/Bread.java
@@ -1,7 +1,7 @@
 class GoldenTouch extends Entity{
 
     public GoldenTouch(){
-        this("Natura\u2122 Bread made with real Gold!", "Bread", 10.d, (short)0);
+        this((short)0);
     }
 
     public GoldenTouch(short qty){
@@ -25,11 +25,11 @@ class GoldenTouch extends Entity{
 class AfricaSecret extends Entity{
 
     public AfricaSecret(){
-        this("B.T.O.\u2122 Africa Secret", "Bread", 2.5d, (short)0);
+        this((short)0);
     }
 
     public AfricaSecret(short qty){
-        this("B.T.O.\u2122 Africa Secret", "Bread", 2.5d, qty);
+        this("B.T.O.\u2122 Africa Secret\t", "Bread", 2.5d, qty);
     }
 
     public AfricaSecret( String name,
@@ -49,11 +49,11 @@ class AfricaSecret extends Entity{
 class DiamondCrumbs extends Entity{
 
     public DiamondCrumbs(){
-        this("Natura\u2122 Diamond Sprinkled Bread", "Bread", 4.d, (short)0);
+        this((short)0);
     }
 
     public DiamondCrumbs(short qty){
-        this("Natura\u2122 Diamond Sprinkled Bread", "Bread", 4.d, qty);
+        this("Natura\u2122 Diamond Sprinkled Bread\t", "Bread", 4.d, qty);
     }
 
     public DiamondCrumbs( String name,
