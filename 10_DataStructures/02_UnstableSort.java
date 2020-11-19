5b403a9dce3fd6e10ca38a502b7df572af4deeff
dargyris <diamantisargiris@gmail.com>
2020-11-19 15:55:20 +0100

    Stable vs Unstable sort algorithms.

---
+++ 10_DataStructures/02_UnstableSort.java
@@ -0,0 +1,14 @@
+class UnstableSort{
+    public static void main ( String[] args ){
+
+        System.out.println( "UNSTABLE SORT" );
+        System.out.println( " * For duplicate values in my data." );
+        System.out.println( "   Will the original ordering of the values be preserved?" );
+        System.out.println( "   Unstable DOES NOT preserve original order." );
+        System.out.println( " * A stable sort is preferred." );
+        System.out.println( " * Bubble sort is a stable sort algorithm." );
+        System.out.println( " * It's easy to degrade stable to an unstable sort..." );
+        System.out.println( "   Eg. in the bubble-sort if, instead of >, use >=." );
+
+    }
+}
