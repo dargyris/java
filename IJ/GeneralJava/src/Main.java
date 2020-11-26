d44e73f96e0a1d356f0fe6c138a7772ddf6bae49
dargyris <diamantisargiris@gmail.com>
2020-11-26 13:44:04 +0100

    Housekeeping.

--- IJ/GeneralJava/src/Main.java
+++ IJ/GeneralJava/src/Main.java
@@ -1,45 +1,110 @@
-class Bicycle {
-    public int gear;
-    public int speed;
+class LinkedList {
+    private Node head;
+    private int length = 0;
 
-    public Bicycle(int gear, int speed) {
-        this.gear = gear;
-        this.speed = speed;
+    static class Node {
+        private Object data;
+        Node next;
+
+        public Node(Object data) {
+            this.data = data;
+            next = null;
+        }
+
+        public Object getData() {
+            return data;
+        }
+
+        public void setData(Object data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
     }
 
-    public void applyBrake(int decrement) {
-        speed -= decrement;
+
+    public LinkedList() {
+        this.head = new Node(null);
     }
 
-    public void speedUp(int increment) {
-        speed += increment;
+    public int size() {
+        return length;
     }
 
-    public String toString() {
-        return "Gears: " + gear + "\nspeed: " + speed;
+    public void add(Object data) {
+        Node node = new Node(data);
+        Node iterator = head;
+        while (iterator.getNext() != null) {
+            iterator = iterator.getNext();
+        }
+        iterator.setNext(node);
+        length++;
+    }
+
+    public Object get(int index) {
+        if (head.getNext() == null || index >= length) {
+            return null;
+        }
+
+        Node iterator = head.getNext();
+        int counter = 0;
+        while (counter < index) {
+            iterator = iterator.getNext();
+            counter++;
+        }
+        return iterator.getData();
+    }
+
+    public int indexOf(Object data) {
+        Node node = head;
+        for (int i = 0; i < length; i++) {
+            node = node.getNext();
+            if (node.getData().equals(data)) {
+                return i;
+            }
+        }
+        return -1; //Throws exception "data not found"
+    }
+
+    public boolean remove(Object data) {
+        if (head.getNext() == null) {
+            return false;
+        }
+
+        Node iterator = head;
+
+        while (iterator.getNext() != null) {
+            if (iterator.getNext().getData().equals(data)) {
+                iterator.setNext(iterator.getNext().getNext());
+                length--;
+                return true;
+            }
+            iterator = iterator.getNext();
+        }
+        return false;
+    }
+
+
+}
+
+class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        System.out.println(list.size());
+        System.out.println(list.get(0));
+        Object object = new Object();
+        list.add(object);
+        System.out.println(list.size());
+        System.out.println(list.get(0));
+
+        testPackage.TestClass.main();
+        testPackage.anotherPackage.AnotherTestClass.main();
     }
 }
 
-class MountainBike extends Bicycle {
-    public int seatHeight;
-    public MountainBike(int gear, int speed, int seatHeight) {
-        super(gear, speed);
-        this.seatHeight = seatHeight;
-	}
-
-    public void setHeight(int newValue) {
-        seatHeight = newValue;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + "\nSeat height: " + seatHeight;
-    }
-}
-
-public class Main {
-    public static void main(String args[]) {
-        MountainBike mb = new MountainBike(3, 100, 25);
-        System.out.println(mb.toString());
-    }
-}
---
+++ IJ/GeneralJava/src/testPackage/anotherPackage/AnotherTestClass.java
@@ -0,0 +1,7 @@
+package testPackage.anotherPackage;
+
+public class AnotherTestClass {
+    public static void main() {
+        System.out.println("In testPackage.anotherPackage!");
+    }
+}
