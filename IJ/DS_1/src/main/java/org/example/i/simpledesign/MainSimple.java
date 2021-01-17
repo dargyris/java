1b730933da4f78be5e2f73573969e9caf962b06f
dargyris <diamantisargiris@gmail.com>
2021-01-17 22:45:12 +0100

    CleanCode for a student grading class.

--- IJ/DS_1/src/main/java/org/example/i/simpledesign/MainSimple.java
+++ IJ/DS_1/src/main/java/org/example/i/simpledesign/MainSimple.java
@@ -1,2 +1,14 @@
-package org.example.i.simpledesign;public class MainSimple {
+package org.example.i.simpledesign;
+
+/* Simple Design: The simple parts of the program that are easy to change. Eg. Class names
+/  Architecture: The approach of the project. Eg. Spring framework.
+**/
+
+/* Four Principles of Simple Design
+/  1. Runs all tests: Code should work. Automated Tests. Test Driven Development TDD.
+/  2. Minimize Duplication: Avoid duplicate code.
+/  3. Maximize Clarity: Naming. Constants. Vars. Packages.
+/  4. Keep it Small:
+**/
+public class MainSimple {
 }
--- IJ/DS_1/src/main/java/org/example/i/simpledesign/StudentHelper.java
+++ IJ/DS_1/src/main/java/org/example/i/simpledesign/StudentHelper.java
@@ -1,2 +1,47 @@
-package org.example.i.simpledesign;public class StudentHelper {
+package org.example.i.simpledesign;
+
+public class StudentHelper {
+
+    private static final int GRADE_B_LOWER_LIMIT = 51;
+    private static final int GRADE_B_UPPER_LIMIT = 80;
+    private static final int EXTRA_POINTS_FOR_MATH = 10;
+
+    public boolean isGradeB(int marks, boolean isMaths) {
+        int extraLimit = isMaths ? EXTRA_POINTS_FOR_MATH : 0;
+        int upperLimit = GRADE_B_UPPER_LIMIT + extraLimit;
+
+        return marks >= GRADE_B_LOWER_LIMIT && marks <= upperLimit;
+    }
+
+    public String getGrade(int mark, boolean isMaths) {
+        String grade = "C";
+
+        if (isGradeA(mark, isMaths))
+            grade = "A";
+        else if (isBGrade(mark, isMaths)) {
+            grade = "B";
+        }
+        return grade;
+    }
+
+    private boolean isGradeA(int mark, boolean isMaths) {
+        int lowerLimitForAGrade = isMaths ? 95
+                : 90;
+        return mark > lowerLimitForAGrade;
+    }
+
+    private boolean isBGrade(int mark, boolean isMaths) {
+        int lowerLimitGradeB = isMaths ? 55
+                : 50;
+        return mark > lowerLimitGradeB && mark < 90;
+    }
+
+    public String willQualifyForQuiz(int marks1, int marks2, boolean isMaths) {
+        if ((isMaths ? marks1 <= 25 : marks1 <= 20)
+                || (isMaths ? marks2 <= 25 : marks2 <= 20)) return "NO";
+        if ((isMaths ? marks1 >= 85 : marks1 >= 80)
+                || (isMaths ? marks2 >= 85 : marks2 >= 80)) return "YES";
+        return "MAYBE";
+    }
+
 }
