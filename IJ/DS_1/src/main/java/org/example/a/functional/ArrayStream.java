b4fedd0ab4230a0475a678a3290e4c38983d4eb4
dargyris <diamantisargiris@gmail.com>
2021-01-25 16:35:18 +0100

    Understanding Arrays.stream.

--- IJ/DS_1/src/main/java/org/example/a/functional/ArrayStream.java
+++ IJ/DS_1/src/main/java/org/example/a/functional/ArrayStream.java
@@ -1,2 +1,41 @@
-package org.example.a.functional;public class ArrayStream {
+package org.example.a.functional;
+
+import org.example.a._refactor.conditional.MenuItem;
+import org.example.a._refactor.conditional.Role;
+import org.example.agnar.goodprogrammer.Constants;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayStream {
+    public void setAuthorizationsInEachMenus(List<MenuItem> menuItemsList, Role[] roles) {
+        if (roles == null) {
+            return;
+        }
+
+        for (MenuItem menuItem : menuItemsList) {
+            if (Arrays.stream(roles).anyMatch(role -> role.getName().equals(menuItem.getReadAccessRole()))) {
+                menuItem.setAccess(Constants.READ);
+                menuItem.setVisible(true);
+            }
+
+            if (Arrays.stream(roles).anyMatch(role -> role.getName().equals(menuItem.getWriteAccessRole()))) {
+                menuItem.setAccess(Constants.WRITE);
+                menuItem.setVisible(true);
+            }
+        }
+    }
+//        for (menuItem : menuItemsList) {
+//            for(role : roles) {
+//                if (role.getName().equals(menuItem.menuItem.getWriteAccessRole())) {
+//                    menuItem.setAccess(Constants.WRITE);
+//                    menuItem.setVisible(true);
+//                }
+//
+//                if (role.getName().equals(menuItem.getReadAccessRole())) {
+//                    menuItem.setAccess(Constants.READ);
+//                    menuItem.setVisible(true);
+//                }
+//            }
+//        }
 }
